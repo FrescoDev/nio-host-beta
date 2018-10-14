137ee03700b6c3c2a7b3ac7e0bf14603877214bd
kojo-hinson <kojo.hinson@officeadminsmbp.home>
2018-10-14 13:39:37 +0100

    fix var name

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const bunyanMiddleware = require("bunyan-middleware");
 const helmet = require("helmet");
 const bunyan = require("bunyan");
-const { $PORT } = process.env;
+const { PORT } = process.env;
 const contracts = require("./contracts");
 const R = require("ramda");
 
@@ -47,8 +47,8 @@ const mapContractsToRoutes = R.map(R.ifElse(isGET, mapToGet, mapToPost));
 
 mapContractsToRoutes(contracts);
 
-app.listen($PORT, () => {
-  logger.info("Server running on port %d", $PORT);
+app.listen(PORT, () => {
+  logger.info("Server running on port %d", PORT);
 });
 
 module.exports = app;
